f004fea68c08e0dc37eb72201e375cc7e07a7f9a
Cristian Rubi <rcristianrubi@gmail.com>
2023-10-04 17:57:54 -0600

    avara.py well implmented

--- avara.py
+++ avara.py
@@ -5,6 +5,26 @@ class State:
         self.chils=chils
         self.f=f
 
+        
+dlr =[[0,1.8,2.5,2.8,4.5,5.0,3.0,5.5,5.8,6.4],
+      [1.8,0,4.4,5.6,3.5,2.3,4.5,3.2,8.6,10.8],
+      [2.5,4.4,0,3.8,3.4,8.0,6.3,7.7,4.0,9.5],
+      [2.8,5.6,3.8,0,7.7,8.0,2.8,10.0,3.0,4.6],
+      [4.5,3.5,3.4,7.7,0,6.7,6.8,4.0,8.7,13.5],
+      [5.0,2.3,8.0,8.0,6.7,0,5.3,3.5,11.8,12.3],
+      [3.0,4.5,6.3,2.8,6.8,5.3,0,6.6,7.5,6.8],
+      [5.5,3.2,7.7,10.0,4.0,3.5,6.6,0,12.5,15.2],
+      [5.8,8.6,4.0,3.0,8.7,11.8,7.5,12.5,0,6.4],
+      [6.4,10.8,9.5,4.6,13.5,12.3,6.8,15.2,6.4,0]]
+
+
+def initValues():
+    for i in range(0,len(ListStates)):
+        if EndS[0].name == ListStates[i].name:
+            for j in range(0,10):
+                ListStates[j].f=dlr[i][j]
+
+
 #Implementacion de estados personalizados
 
 #print("Ingrese el valor heuristico para A")
@@ -173,6 +193,9 @@ OpenL = []
 CloseL = [A]
 CloseL[0]=CurrentS[0]
 print(CloseL[0].name)
+
+initValues()
+
 findS=BusquedaAvara()
 
 if findS == True:
